b062ecde4c71e6f7c7d1661a3884b97bfd8a3580
jongyun.koo <jongyun.koo@gmail.com>
2019-04-02 15:20:22 +0530

    add solution 5721 in back jun

---
+++ src/com/company/DP/Sol5721.java
@@ -0,0 +1,26 @@
+package com.company.DP;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Sol5721 {
+
+    static int rowCnt;
+    static int colCnt;
+    static int xLine;
+    static int yLine;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            rowCnt = sc.nextInt();
+            colCnt = sc.nextInt();
+            if (rowCnt == 0 && colCnt == 0) {
+                return;
+            } else {
+
+            }
+        }
+    }
+}
