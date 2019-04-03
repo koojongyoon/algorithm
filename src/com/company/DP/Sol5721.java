d30d2ec5e78c5b22af15f164b422d2bce11c9dc1
jongyun.koo <jongyun.koo@gmail.com>
2019-04-03 10:27:57 +0530

    add solution

--- src/com/company/DP/Sol5721.java
+++ src/com/company/DP/Sol5721.java
@@ -1,25 +1,45 @@
 package com.company.DP;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Sol5721 {
 
     static int rowCnt;
     static int colCnt;
-    static int xLine;
-    static int yLine;
+    static int xLine[];
+    static int yLine[];
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         while (true) {
+
             rowCnt = sc.nextInt();
             colCnt = sc.nextInt();
+
             if (rowCnt == 0 && colCnt == 0) {
                 return;
             } else {
+                yLine = new int[rowCnt+1];
 
+                for (int i = 1; i <= rowCnt; i++) {
+                    xLine = new int[colCnt+1];
+                    for (int k = 1; k <= colCnt; k++) {
+                        xLine[k] = sc.nextInt();
+                        if (k >= 2) {
+                            xLine[k] = Math.max(xLine[k-2] + xLine[k], xLine[k-1]);
+                        }
+                    }
+
+                    yLine[i] = xLine[colCnt];
+
+                    if (i >= 2) {
+                        yLine[i] = Math.max(yLine[i-2] + yLine[i], yLine[i-1]);
+                    }
+                }
+
+                System.out.println(yLine[rowCnt]);
             }
         }
     }
---
+++ src/com/company/Uncategorized/Sol1159.java
@@ -0,0 +1,46 @@
+package com.company.Uncategorized;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Sol1159 {
+
+    public static void main(String[] args) throws IOException {
+
+        String sol = "";
+
+        Map map = new HashMap<String, Integer>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String[] name = br.readLine().split("");
+            int count = map.get(name[0]) == null ? 0 : (int) map.get(name[0]);
+            map.put(name[0], count + 1);
+        }
+
+        ArrayList alphabet = new ArrayList<String>();
+        for (Object k : map.keySet()) {
+            String key = k.toString();
+            if ((int) map.get(key) > 4) {
+                alphabet.add(key.toCharArray()[0]);
+            }
+        }
+
+        Collections.sort(alphabet);
+        for (Object k : alphabet) {
+            String value = k.toString();
+            sol = sol + value;
+
+        }
+
+        if (!sol.equals("")) {
+            System.out.println(sol);
+        } else {
+            System.out.println("PREDAJA");
+        }
+    }
+}
