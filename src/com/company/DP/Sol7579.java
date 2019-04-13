549a740134d8410864162ac428567e8100b6ba6f
carl.koo <jongyun.koo@gmail.com>
2019-04-13 16:54:25 +0900

    'add'

--- src/com/company/DP/Sol7579.java
+++ src/com/company/DP/Sol7579.java
@@ -3,43 +3,40 @@ package com.company.DP;
 import java.io.*;
 
 public class Sol7579 {
-
-    static App[] appList;
-
-    static class App implements Comparable<App> {
-        int index;
-        int memory;
-        int cost;
-
-        App (int index, int memory, int cost) {
-            this.index = index;
-            this.memory = memory;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(App o) {
-            return this.cost - o.cost;
-        }
-    }
+    static int[] memory;
+    static int[] cost;
+    static int[] appMemory;
+    static int sum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] countMemory = br.readLine().split(" ");
 
         int count = Integer.parseInt(countMemory[0]);
         long needMemory = Long.parseLong(countMemory[1]);
-        appList = new App[count];
 
         String[] inputAppMemory = br.readLine().split(" ");
         String[] inputAppCost = br.readLine().split(" ");
+        memory = new int[count];
+        cost = new int[count];
+        appMemory = new int[10001];
 
         for (int i = 0; i < count; i++) {
-            appList[i] = new App(i, Integer.parseInt(inputAppMemory[i]) , Integer.parseInt(inputAppCost[i]));
+            memory[i] = Integer.parseInt(inputAppMemory[i]);
+            cost[i] = Integer.parseInt(inputAppCost[i]);
+            sum = sum + cost[i];
         }
 
+        for (int i = 0; i < count; i++) {
+            for (int j = sum; j >= cost[i]; j--) {
+                appMemory[j] = Math.max(appMemory[j], appMemory[j - cost[i]] + memory[i]);
+            }
+        }
 
+        int i = 0;
+        for (i = 0; i < sum && appMemory[i] < needMemory; i++); {
+            System.out.println(String.valueOf(i));
+            return;
+        }
     }
-
 }
