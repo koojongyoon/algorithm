04c9e105f2071d93d1ee26278b88ef4f64d53c13
hayden <soar0706@gmail.com>
2019-04-24 17:18:32 +0900

    add sol 1389

--- src/com/company/Graph/Sol1389.java
+++ src/com/company/Graph/Sol1389.java
@@ -3,7 +3,6 @@ package com.company.Graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Sol1389 {
 
@@ -21,35 +20,43 @@ public class Sol1389 {
         int people = Integer.parseInt(peopleRelation[0]);
         int relation = Integer.parseInt(peopleRelation[1]);
 
-        map = new boolean[people+1][people+1];
         costMap = new int[people+1][people+1];
-
-        for (int i = 0; i < people+1; i++) {
-           for (int k = 0; k < people+1; k++) {
-               if (i == k) {
-                   costMap[i][k] = 0;
-               } else {
-                   costMap[i][k] = MAX_VALUE;
-               }
-           }
-        }
 
         for (int i = 0; i < relation; i++) {
             String[] fromTo = br.readLine().split(" ");
             int from = Integer.parseInt(fromTo[0]);
             int to = Integer.parseInt(fromTo[1]);
-            map[from][to] = true;
             costMap[from][to] = 1;
+            costMap[to][from] = 1;
+        }
+
+        for (int i = 1; i < people+1; i++) {
+            for (int k = 1; k < people+1; k++) {
+                if (i == k) {
+                    costMap[i][k] = 0;
+                } else {
+                    if (costMap[i][k] != 1) {
+                        costMap[i][k] = MAX_VALUE;
+                    }
+                }
+            }
         }
 
         for (int k = 1; k < people+1; k++) {
             for (int i = 1; i < people+1; i++) {
                 for (int j = 1; j < people+1; j++) {
                     if (costMap[i][j] > costMap[i][k] + costMap[k][j]) {
-                        costMap[i][j] =
+                        costMap[i][j] = costMap[i][k] + costMap[k][j];
                     }
                 }
             }
+        }
+
+        for (int i = 1; i < people+1; i++) {
+            for (int k = 1; k < people+1; k++) {
+                System.out.print(costMap[i][k] + " ");
+            }
+            System.out.println();
         }
     }
 }
