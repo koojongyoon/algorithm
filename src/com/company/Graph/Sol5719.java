b5c27ab5db33b90f33af6479e36318b1963188f1
hayden <soar0706@gmail.com>
2019-04-29 11:30:55 +0900

    add sol 5719

--- src/com/company/Graph/Sol5719.java
+++ src/com/company/Graph/Sol5719.java
@@ -3,16 +3,20 @@ package com.company.Graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Sol5719 {
 
     static int INF = 100000001;
     static int[] dest;
-    static int[] by;
+    static int[][] map;
+    static List<Integer>[] trace;
     static ArrayList<Edge>[] list;
-    static boolean[] visit;
+    static int[] byArr;
+    static int N;
+    static int M;
+    static int start;
+    static int end;
 
     static class Edge implements Comparable<Edge>{
         int next;
@@ -35,22 +39,25 @@ public class Sol5719 {
         String[] NM;
 
         while ((NM = br.readLine().split(" ")).length > 0) {
-            int N = Integer.parseInt(NM[0]);
-            int M = Integer.parseInt(NM[1]);
+            N = Integer.parseInt(NM[0]);
+            M = Integer.parseInt(NM[1]);
 
             list = new ArrayList[N];    //각 시작점에서 갈수 있는 목적지의 edge종
             dest = new int[N]; //N까지의 거리
-            by = new int[N];
-            visit = new boolean[N];   // 방문여부 표시
+            byArr = new int[N];
+            map = new int[N][N];
+            trace = new ArrayList[N];
 
             for (int i = 0; i < N; i++) {
                 list[i] = new ArrayList<>();
                 dest[i] = INF;
+                Arrays.fill(map[i], -1);
+                trace[i] = new ArrayList<>();
             }
 
             String[] startEnd = br.readLine().split(" ");
-            int start = Integer.parseInt(startEnd[0]);
-            int end = Integer.parseInt(startEnd[1]);
+            start = Integer.parseInt(startEnd[0]);
+            end = Integer.parseInt(startEnd[1]);
 
             dest[start] = 0;
 
@@ -59,25 +66,43 @@ public class Sol5719 {
                 int u = Integer.parseInt(UVP[0]);
                 int v = Integer.parseInt(UVP[1]);
                 int p = Integer.parseInt(UVP[2]);
-
+                map[u][v] = p;
                 list[u].add(new Edge(v, p));
             }
 
-            PriorityQueue<Edge> queue = new PriorityQueue<>();
-            queue.add(new Edge(start, 0));
+            dijkstra(start);
+            traceback();
+            Arrays.fill(dest, INF);
+            dijkstra(start);
 
-            while(!queue.isEmpty()) {
-                Edge e = queue.poll();
-                if (visit[e.next]) {
-                    continue;
+            System.out.println(dest[end] >= INF ? -1 : dest[end]);
+        }
+    }
+
+    private static void dijkstra(int start) {
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        queue.add(new Edge(start, 0));
+
+        while(!queue.isEmpty()) {
+            Edge e = queue.poll();
+            for (int next = 0; next < N; next++) {
+                if(map[e.next][next] == -1 && e.weight + map[e.next][next] <= dest[next]) {
+                    queue.add(new Edge(next, dest[next] = e.weight + map[e.next][next]));
+                    trace[next].add(e.next);
                 }
-                visit[e.next] = true;
+            }
+        }
+    }
 
-                for (Edge edge : list[e.next]) {
-                    if (!visit[edge.next]) {
-                        dest[edge.next] = Math.min(dest[edge.next], dest[e.next] + edge.weight);
-                        queue.add(new Edge(edge.next, dest[edge.next]));
-                    }
+    private static void traceback() {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(end);
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for (Integer pre : trace[cur]) {
+                if (map[pre][cur] != -1 && dest[cur] == map[pre][cur] + dest[pre]) {
+                    q.offer(pre);
+                    map[pre][cur] = -1;
                 }
             }
         }
