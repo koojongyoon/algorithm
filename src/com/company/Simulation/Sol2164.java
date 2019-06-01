99d9f65ede50497882fc42b12b6d5eb39b85bbf6
구종윤 <koojongyun@gujong-yun-ui-MacBookPro.local>
2019-06-01 15:11:12 +0900

    add sol 2164

---
+++ src/com/company/Simulation/Sol2164.java
@@ -0,0 +1,20 @@
+package com.company.Simulation;
+
+import java.util.Scanner;
+
+public class Sol2164 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = 1;
+
+        for (int i = 0; k <= N; i++) {
+            k = k + (int) Math.pow(2, i);
+        }
+
+        int j = (N - k - 1) * 2;
+
+        System.out.println(j);
+    }
+}
