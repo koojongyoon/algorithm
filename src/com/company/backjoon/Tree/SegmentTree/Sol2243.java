a9885226207b09694c07f8d0f9a21e714bf9710d
actmember <actmember@gujong-yun-ui-MacBookPro.local>
2019-08-06 22:11:41 +0900

    add sol

--- src/com/company/backjoon/Tree/SegmentTree/Sol2243.java
+++ src/com/company/backjoon/Tree/SegmentTree/Sol2243.java
@@ -8,8 +8,10 @@ public class Sol2243 {
 
     static long[] arr;
     static long[] tree;
-    static int SIZE = 1000001;
+    static int SIZE = 50;
+    static long H;
     static long result = 0;
+    static long treeSize = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,15 +19,17 @@ public class Sol2243 {
         int n = Integer.parseInt(br.readLine());
         arr = new long[SIZE+1];
         tree = new long[4*SIZE];
+        H = 1<< (int) Math.ceil(Math.log(SIZE)/Math.log(2));
+        treeSize = (1 << (1 + H));
+
         for (int i = 0; i < n; i++) {
             String[] inputStr = br.readLine().split(" ");
             int a = Integer.parseInt(inputStr[0]);
             int idx = Integer.parseInt(inputStr[1]);
             if (a == 1) {
                 long getIndex = pickCandy(1, 1, SIZE, idx);
-                System.out.println(getIndex);
-                putCandy(1, 1, SIZE, getIndex, -1);
                 result = 0;
+                putCandy(1, 1, SIZE, getIndex, -1);
             } else {
                 int putCnt = Integer.parseInt(inputStr[2]);
                 putCandy(1, 1, SIZE, idx, putCnt);
@@ -35,25 +39,31 @@ public class Sol2243 {
 
     private static long pickCandy(int index, long start, long end, long updateIndex) {
         if (start == end && result == 0) {
+            System.out.println(start);
             return start;
         }
         long mid = (start + end) / 2;
 
-        if (result == 0 && (index*2 <= SIZE && tree[index*2] >= updateIndex)) {
+        if (result == 0 && (index*2 <= treeSize && tree[index*2] >= updateIndex)) {
             return result = pickCandy(index*2, start, mid, updateIndex);
         }
+
         updateIndex = updateIndex - tree[index*2];
-        if (result == 0 && (index*2 + 1 <= SIZE && tree[index*2+1] >= updateIndex)) {
+
+        if (result == 0 && (index*2 + 1 <= treeSize && tree[index*2+1] >= updateIndex)) {
             return result = pickCandy(index*2+1, mid+1, end, updateIndex);
         }
         return 0;
     }
 
     private static void putCandy(int index, long start, long end, long updateIndex, long updateValue) {
+
         if (updateIndex < start || updateIndex > end) {
             return;
         }
+
         tree[index] = tree[index] + updateValue;
+
         if (start != end) {
             long mid = (start + end) / 2;
             putCandy(index * 2, start, mid, updateIndex, updateValue);
