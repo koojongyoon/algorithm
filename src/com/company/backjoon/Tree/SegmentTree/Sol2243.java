a606198a8ecb9ca44044a3dd6fba6e623636af2a
actmember <actmember@gujong-yun-ui-MacBookPro.local>
2019-08-08 22:53:47 +0900

    add sol

--- src/com/company/backjoon/Tree/SegmentTree/Sol2243.java
+++ src/com/company/backjoon/Tree/SegmentTree/Sol2243.java
@@ -14,7 +14,7 @@ public class Sol2243 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        tree = new long[N+1];
+        tree = new long[4*N];
 
         for (int i = 0; i < N; i++) {
             String[] inputStr = br.readLine().split(" ");
@@ -22,8 +22,8 @@ public class Sol2243 {
             result = 0;
             if (command == 1) {
                 int pickIndex = Integer.parseInt(inputStr[1]);
-                pick(1, 1, N, pickIndex);
-                put(1, 1, N, pickIndex, -1);
+                long getIndex = pick(1, 1, N, pickIndex);
+                System.out.println(put(1, 1, N, getIndex, -1));
             } else {
                 int putIndex = Integer.parseInt(inputStr[1]);
                 int putCount = Integer.parseInt(inputStr[2]);
@@ -32,26 +32,37 @@ public class Sol2243 {
         }
     }
 
-    private static long pick(int index, int start, int end, int updateIndex) {
+    private static long pick(int index, int start, int end, int pickIndex) {
         if (index < start || index > end) {
             return 0;
         }
-        if (updateIndex > start && updateIndex < end) {
+        if (pickIndex >= start && pickIndex <= end) {
+            int middle = (start + end)/2;
+            if (pick(index*2, start, middle, pickIndex) < pickIndex) {
+                pick(index*2, middle+1, end, pickIndex);
+            }
+            if (pick(index*2, middle+1, end, pickIndex) > pickIndex) {
+                pick(index*2, start, middle+1, pickIndex);
+            }
             return start;
         }
         int mid = (start + end)/2;
-        return tree[index] = pick(index*2, start, mid, updateIndex) + pick(index*2+1, mid+1, start, updateIndex);
+        return tree[index] = pick(index*2, start, mid, pickIndex) + pick(index*2+1, mid+1, start, pickIndex);
     }
 
-    private static long put(int index, int start, int end, int updateIndex, int updateCount) {
-        if (index < start || index > end) {
+    private static long put(int index, int start, int end, long updateIndex, int updateCount) {
+        if (updateIndex < start || updateIndex > end) {
             return 0;
         }
-        if (updateIndex > start && updateIndex < end) {
-            tree[index] = tree[index] + updateCount;
-            return start;
+
+        tree[index] = tree[index] + updateCount;
+
+        if (start != end) {
+            int mid = (start + end)/2;
+            put(index*2, start, mid, updateIndex, updateCount);
+            put(index*2+1, mid+1, end, updateIndex, updateCount);
+            return tree[index] = tree[index*2] + tree[index*2+1];
         }
-        int mid = (start + end)/2;
-        return tree[index] = put(index*2, start, mid, updateIndex, updateCount) + put(index*2+1, mid+1, start, updateIndex, updateCount);
+        return 0;
     }
 }
