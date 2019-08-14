8a171741a1e0b8462118d5ca061f42fb1907f15f
actmember <actmember@gujong-yun-ui-MacBookPro.local>
2019-08-14 21:32:21 +0900

    add

--- src/com/company/backjoon/Tree/SegmentTree/midvalue/Sol9426.java
+++ src/com/company/backjoon/Tree/SegmentTree/midvalue/Sol9426.java
@@ -20,7 +20,7 @@ public class Sol9426 {
         N = Integer.parseInt(inputArr[0]);
         K =  Integer.parseInt(inputArr[1]);
         arr = new int[MAX_N];
-        tree = new int[4*MAX_N-1];
+        tree = new int[4 * MAX_N];
         totalSum = 0;
 
         for (int n = 0; n < N; n++) {
@@ -33,7 +33,7 @@ public class Sol9426 {
 
         for (int i = K - 1; i <  N; i++) {
             update(1, 0, MAX-1, arr[i], 1);
-            totalSum = totalSum + query( 1, 0, MAX-1, (K+1) / 2);
+            totalSum = totalSum + query( 1, 0, MAX-1, (K+1)/2);
             update(1, 0, MAX-1, arr[i - K + 1], -1);
         }
         System.out.println(totalSum);
@@ -51,10 +51,11 @@ public class Sol9426 {
     }
 
     private static long query (int index, long start, long end, long value) {
-        long mid = (start + end) / 2;
         if (start == end) {
             return start;
         }
+
+        long mid = (start + end) / 2;
         if (tree[index * 2] >= value) {
             return query(index*2, start, mid, value);
         }
